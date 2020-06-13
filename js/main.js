ce72a137df419e81381b56cee528d1767f2c6e6c
barabum666 <65859711+barabum666@users.noreply.github.com>
2020-06-13 21:15:38 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
+	//Burger
 	$('.menu-toggle').click(function(event){
 		$('.menu-toggle,.main-navigation').toggleClass('active');
+	});
+
+	//Animation
+	var windowHeight = $(window).height();
+	var el = document.querySelectorAll('#animated');
+	$(document).on('scroll', function() {
+		$(el).each(function() {
+			var self = $(this),
+			height = self.offset().top + (self.height() / 4);
+			if ($(document).scrollTop() + windowHeight >= height) {
+				self.addClass('animated')
+			}
+		});
 	});
 });
